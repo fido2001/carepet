2063402a5cd42bd09ebb3bfce8567f946e329be3
fido2001 <firdauspriyono17@gmail.com>
2020-10-27 17:22:03 +0700

    Mengganti tampilan register

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -1,119 +1,125 @@
-@extends('layouts.app')
+<!DOCTYPE html>
+<html lang="en">
+<head>
+<meta charset="UTF-8">
+<meta content="width=device-width, initial-scale=1, maximum-scale=1, shrink-to-fit=no" name="viewport">
+<title>Register &mdash; Care Pet</title>
 
-@section('content')
-<div class="container">
-    <div class="row justify-content-center">
-        <div class="col-md-8">
-            <div class="card">
-                <div class="card-header">{{ __('Register') }}</div>
+<!-- General CSS Files -->
+<link rel="stylesheet" href="{{ asset('../assets/modules/bootstrap/css/bootstrap.min.css') }}">
+<link rel="stylesheet" href="{{ asset('../assets/modules/fontawesome/css/all.min.css') }}">
 
-                <div class="card-body">
-                    <form method="POST" action="{{ route('register') }}">
-                        @csrf
+<!-- CSS Libraries -->
+<link rel="stylesheet" href="{{ asset('../assets/modules/jquery-selectric/selectric.css') }}">
 
-                        <div class="form-group row">
-                            <label for="name" class="col-md-4 col-form-label text-md-right">{{ __('Name') }}</label>
+<!-- Template CSS -->
+<link rel="stylesheet" href="{{ asset('../assets/css/style.css') }}">
+<link rel="stylesheet" href="{{ asset('../assets/css/components.css') }}">
+<!-- Start GA -->
+<script async src="https://www.googletagmanager.com/gtag/js?id=UA-94034622-3"></script>
+<script>
+window.dataLayer = window.dataLayer || [];
+function gtag(){dataLayer.push(arguments);}
+gtag('js', new Date());
 
-                            <div class="col-md-6">
-                                <input id="name" type="text" class="form-control @error('name') is-invalid @enderror" name="name" value="{{ old('name') }}" required autocomplete="name" autofocus>
+gtag('config', 'UA-94034622-3');
+</script>
+<!-- /END GA --></head>
 
-                                @error('name')
-                                    <span class="invalid-feedback" role="alert">
-                                        <strong>{{ $message }}</strong>
-                                    </span>
-                                @enderror
-                            </div>
-                        </div>
-                        
-                        <div class="form-group row">
-                            <label for="username" class="col-md-4 col-form-label text-md-right">Username</label>
-
-                            <div class="col-md-6">
-                                <input id="username" type="text" class="form-control @error('username') is-invalid @enderror" name="username" value="{{ old('username') }}" required autocomplete="username" autofocus>
-
-                                @error('username')
-                                    <span class="invalid-feedback" role="alert">
-                                        <strong>{{ $message }}</strong>
-                                    </span>
-                                @enderror
-                            </div>
+<body>
+    <div id="app">
+        <section class="section">
+            <div class="container mt-5">
+                <div class="row">
+                    <div class="col-12 col-sm-10 offset-sm-1 col-md-8 offset-md-2 col-lg-8 offset-lg-2 col-xl-8 offset-xl-2">
+                        <div class="login-brand">
+                            <img src="{{ asset('../assets/img/CP-Logo.png') }}" alt="logo" width="100">
                         </div>
 
-                        <div class="form-group row">
-                            <label for="email" class="col-md-4 col-form-label text-md-right">{{ __('E-Mail Address') }}</label>
+                        <div class="card card-primary">
+                            <div class="card-header"><h4>Register</h4></div>
 
-                            <div class="col-md-6">
-                                <input id="email" type="email" class="form-control @error('email') is-invalid @enderror" name="email" value="{{ old('email') }}" required autocomplete="email">
+                            <div class="card-body">
+                                <form method="POST" action="{{ route('register') }}">
+                                    @csrf
+                                    <div class="row">
+                                        <div class="form-group col-6">
+                                            <label for="name">Full Name</label>
+                                            <input id="name" type="text" class="form-control" name="name" autofocus>
+                                        </div>
+                                        <div class="form-group col-6">
+                                            <label for="username">Username</label>
+                                            <input id="username" type="text" class="form-control" name="username">
+                                        </div>
+                                    </div>
 
-                                @error('email')
-                                    <span class="invalid-feedback" role="alert">
-                                        <strong>{{ $message }}</strong>
-                                    </span>
-                                @enderror
+                                    <div class="form-group">
+                                    <label for="email">Email</label>
+                                    <input id="email" type="email" class="form-control" name="email">
+                                    <div class="invalid-feedback">
+                                    </div>
+                                    </div>
+
+                                    <div class="row">
+                                    <div class="form-group col-6">
+                                        <label for="password" class="d-block">Password</label>
+                                        <input id="password" type="password" class="form-control pwstrength" data-indicator="pwindicator" name="password">
+                                        <div id="pwindicator" class="pwindicator">
+                                        <div class="bar"></div>
+                                        <div class="label"></div>
+                                        </div>
+                                    </div>
+                                    <div class="form-group col-6">
+                                        <label for="password-confirm" class="d-block">Password Confirmation</label>
+                                        <input id="password-confirm" type="password" class="form-control" name="password_confirmation">
+                                    </div>
+                                    </div>
+
+                                    <div class="row">
+                                        <div class="form-group col-12">
+                                            <label for="noHp" class="d-block">Nomor HP</label>
+                                            <input id="noHp" type="text" class="form-control" name="noHp">
+                                        </div>
+                                    </div>
+                                    <div class="row">
+                                        <div class="form-group col-12">
+                                            <label for="alamat" class="d-block">Alamat</label>
+                                            <textarea id="alamat" type="text" class="form-control" name="alamat"></textarea>
+                                        </div>
+                                    </div>
+
+                                    <div class="form-group">
+                                    <button type="submit" class="btn btn-info btn-lg btn-block">
+                                        Register
+                                    </button>
+                                    </div>
+                                </form>
                             </div>
                         </div>
-                        
-                        <div class="form-group row">
-                            <label for="noHp" class="col-md-4 col-form-label text-md-right">Nomor HP</label>
-
-                            <div class="col-md-6">
-                                <input id="noHp" type="text" class="form-control @error('noHp') is-invalid @enderror" name="noHp" value="{{ old('noHp') }}" required>
-
-                                @error('noHp')
-                                    <span class="invalid-feedback" role="alert">
-                                        <strong>{{ $message }}</strong>
-                                    </span>
-                                @enderror
-                            </div>
-                        </div>
-                        
-                        <div class="form-group row">
-                            <label for="alamat" class="col-md-4 col-form-label text-md-right">Alamat</label>
-
-                            <div class="col-md-6">
-                                <textarea id="alamat" type="text" class="form-control @error('alamat') is-invalid @enderror" name="alamat" value="{{ old('alamat') }}" required>
-                                </textarea>
-                                @error('alamat')
-                                    <span class="invalid-feedback" role="alert">
-                                        <strong>{{ $message }}</strong>
-                                    </span>
-                                @enderror
-                            </div>
-                        </div>
-
-                        <div class="form-group row">
-                            <label for="password" class="col-md-4 col-form-label text-md-right">{{ __('Password') }}</label>
-
-                            <div class="col-md-6">
-                                <input id="password" type="password" class="form-control @error('password') is-invalid @enderror" name="password" required autocomplete="new-password">
-
-                                @error('password')
-                                    <span class="invalid-feedback" role="alert">
-                                        <strong>{{ $message }}</strong>
-                                    </span>
-                                @enderror
-                            </div>
-                        </div>
-
-                        <div class="form-group row">
-                            <label for="password-confirm" class="col-md-4 col-form-label text-md-right">{{ __('Confirm Password') }}</label>
-
-                            <div class="col-md-6">
-                                <input id="password-confirm" type="password" class="form-control" name="password_confirmation" required autocomplete="new-password">
-                            </div>
-                        </div>
-
-                        <div class="form-group row mb-0">
-                            <div class="col-md-6 offset-md-4">
-                                <button type="submit" class="btn btn-primary">
-                                    {{ __('Register') }}
-                                </button>
-                            </div>
-                        </div>
-                    </form>
+                    </div>
                 </div>
             </div>
-        </div>
+        </section>
     </div>
-</div>
-@endsection
+
+<!-- General JS Scripts -->
+<script src="{{ asset('../assets/modules/jquery.min.js') }}"></script>
+<script src="{{ asset('../assets/modules/popper.js') }}"></script>
+<script src="{{ asset('../assets/modules/tooltip.js') }}"></script>
+<script src="{{ asset('../assets/modules/bootstrap/js/bootstrap.min.js') }}"></script>
+<script src="{{ asset('../assets/modules/nicescroll/jquery.nicescroll.min.js') }}"></script>
+<script src="{{ asset('../assets/modules/moment.min.js') }}"></script>
+<script src="{{ asset('../assets/js/stisla.js') }}"></script>
+
+<!-- JS Libraies -->
+<script src="{{ asset('../assets/modules/jquery-pwstrength/jquery.pwstrength.min.js') }}"></script>
+<script src="{{ asset('../assets/modules/jquery-selectric/jquery.selectric.min.js') }}"></script>
+
+<!-- Page Specific JS File -->
+<script src="{{ asset('../assets/js/page/auth-register.js') }}"></script>
+
+<!-- Template JS File -->
+<script src="{{ asset('../assets/js/scripts.js') }}"></script>
+<script src="{{ asset('../assets/js/custom.js') }}"></script>
+</body>
+</html>
