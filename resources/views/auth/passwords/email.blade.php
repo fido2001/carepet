285e9a52096797740cdf40ac0cc6b7a932569234
fido2001 <firdauspriyono17@gmail.com>
2020-10-27 22:41:54 +0700

    Mengubah tampilan reset password

--- resources/views/auth/passwords/email.blade.php
+++ resources/views/auth/passwords/email.blade.php
@@ -1,47 +1,93 @@
-@extends('layouts.app')
+<!DOCTYPE html>
+<html lang="en">
+<head>
+<meta charset="UTF-8">
+<meta content="width=device-width, initial-scale=1, maximum-scale=1, shrink-to-fit=no" name="viewport">
+<title>Forgot Password &mdash; Care Pet</title>
 
-@section('content')
-<div class="container">
-    <div class="row justify-content-center">
-        <div class="col-md-8">
-            <div class="card">
-                <div class="card-header">{{ __('Reset Password') }}</div>
+<!-- General CSS Files -->
+<link rel="stylesheet" href="{{ asset('../assets/modules/bootstrap/css/bootstrap.min.css') }}">
+<link rel="stylesheet" href="{{ asset('../assets/modules/fontawesome/css/all.min.css') }}">
 
-                <div class="card-body">
-                    @if (session('status'))
-                        <div class="alert alert-success" role="alert">
-                            {{ session('status') }}
+<!-- CSS Libraries -->
+
+<!-- Template CSS -->
+<link rel="stylesheet" href="{{ asset('../assets/css/style.css') }}">
+<link rel="stylesheet" href="{{ asset('../assets/css/components.css') }}">
+<!-- Start GA -->
+<script async src="https://www.googletagmanager.com/gtag/js?id=UA-94034622-3"></script>
+<script>
+window.dataLayer = window.dataLayer || [];
+function gtag(){dataLayer.push(arguments);}
+gtag('js', new Date());
+
+gtag('config', 'UA-94034622-3');
+</script>
+<!-- /END GA -->
+</head>
+
+<body>
+    <div id="app">
+        <section class="section">
+            <div class="container mt-5">
+                <div class="row">
+                    <div class="col-12 col-sm-8 offset-sm-2 col-md-6 offset-md-3 col-lg-6 offset-lg-3 col-xl-4 offset-xl-4">
+                        <div class="login-brand">
+                            <img src="{{ asset('../assets/img/CP-Logo.png') }}" alt="logo" width="100">
                         </div>
-                    @endif
 
-                    <form method="POST" action="{{ route('password.email') }}">
-                        @csrf
+                        <div class="card card-primary">
+                            @if (session('status'))
+                                <div class="alert alert-success" role="alert">
+                                    {{ session('status') }}
+                                </div>
+                            @endif
+                            <div class="card-header"><h4>Forgot Password</h4></div>
 
-                        <div class="form-group row">
-                            <label for="email" class="col-md-4 col-form-label text-md-right">{{ __('E-Mail Address') }}</label>
+                            <div class="card-body">
+                                <p class="text-muted">We will send a link to reset your password</p>
+                                <form method="POST" action="{{ route('password.email') }}">
+                                    @csrf
 
-                            <div class="col-md-6">
-                                <input id="email" type="email" class="form-control @error('email') is-invalid @enderror" name="email" value="{{ old('email') }}" required autocomplete="email" autofocus>
+                                    <div class="form-group">
+                                        <label for="email">Email</label>
+                                        <input id="email" type="email" class="form-control @error('email') is-invalid @enderror" name="email" value="{{ old('email') }}" tabindex="1" required autofocus>
+                                        @error('email')
+                                            <span class="invalid-feedback" role="alert">
+                                                <strong>{{ $message }}</strong>
+                                            </span>
+                                        @enderror
+                                    </div>
 
-                                @error('email')
-                                    <span class="invalid-feedback" role="alert">
-                                        <strong>{{ $message }}</strong>
-                                    </span>
-                                @enderror
+                                    <div class="form-group">
+                                        <button type="submit" class="btn btn-info btn-lg btn-block" tabindex="4">
+                                            Forgot Password
+                                        </button>
+                                    </div>
+                                </form>
                             </div>
                         </div>
-
-                        <div class="form-group row mb-0">
-                            <div class="col-md-6 offset-md-4">
-                                <button type="submit" class="btn btn-primary">
-                                    {{ __('Send Password Reset Link') }}
-                                </button>
-                            </div>
-                        </div>
-                    </form>
+                    </div>
                 </div>
             </div>
-        </div>
+        </section>
     </div>
-</div>
-@endsection
+
+<!-- General JS Scripts -->
+<script src="{{ asset('../assets/modules/jquery.min.js') }}"></script>
+<script src="{{ asset('../assets/modules/popper.js') }}"></script>
+<script src="{{ asset('../assets/modules/tooltip.js') }}"></script>
+<script src="{{ asset('../assets/modules/bootstrap/js/bootstrap.min.js') }}"></script>
+<script src="{{ asset('../assets/modules/nicescroll/jquery.nicescroll.min.js') }}"></script>
+<script src="{{ asset('../assets/modules/moment.min.js') }}"></script>
+<script src="{{ asset('../assets/js/stisla.js') }}"></script>
+
+<!-- JS Libraies -->
+
+<!-- Page Specific JS File -->
+
+<!-- Template JS File -->
+<script src="{{ asset('../assets/js/scripts.js') }}"></script>
+<script src="{{ asset('../assets/js/custom.js') }}"></script>
+</body>
+</html>
--- resources/views/auth/passwords/reset.blade.php
+++ resources/views/auth/passwords/reset.blade.php
@@ -1,65 +1,108 @@
-@extends('layouts.app')
+<!DOCTYPE html>
+<html lang="en">
+<head>
+<meta charset="UTF-8">
+<meta content="width=device-width, initial-scale=1, maximum-scale=1, shrink-to-fit=no" name="viewport">
+<title>Reset Password &mdash; Stisla</title>
 
-@section('content')
-<div class="container">
-    <div class="row justify-content-center">
-        <div class="col-md-8">
-            <div class="card">
-                <div class="card-header">{{ __('Reset Password') }}</div>
+<!-- General CSS Files -->
+<link rel="stylesheet" href="{{ asset('../assets/modules/bootstrap/css/bootstrap.min.css') }}">
+<link rel="stylesheet" href="{{ asset('../assets/modules/fontawesome/css/all.min.css') }}">
 
-                <div class="card-body">
-                    <form method="POST" action="{{ route('password.update') }}">
-                        @csrf
+<!-- CSS Libraries -->
 
-                        <input type="hidden" name="token" value="{{ $token }}">
+<!-- Template CSS -->
+<link rel="stylesheet" href="{{ asset('../assets/css/style.css') }}">
+<link rel="stylesheet" href="{{ asset('../assets/css/components.css') }}">
+<!-- Start GA -->
+<script async src="https://www.googletagmanager.com/gtag/js?id=UA-94034622-3"></script>
+<script>
+window.dataLayer = window.dataLayer || [];
+function gtag(){dataLayer.push(arguments);}
+gtag('js', new Date());
 
-                        <div class="form-group row">
-                            <label for="email" class="col-md-4 col-form-label text-md-right">{{ __('E-Mail Address') }}</label>
+gtag('config', 'UA-94034622-3');
+</script>
+<!-- /END GA --></head>
 
-                            <div class="col-md-6">
-                                <input id="email" type="email" class="form-control @error('email') is-invalid @enderror" name="email" value="{{ $email ?? old('email') }}" required autocomplete="email" autofocus>
-
-                                @error('email')
-                                    <span class="invalid-feedback" role="alert">
-                                        <strong>{{ $message }}</strong>
-                                    </span>
-                                @enderror
-                            </div>
+<body>
+    <div id="app">
+        <section class="section">
+            <div class="container mt-5">
+                <div class="row">
+                    <div class="col-12 col-sm-8 offset-sm-2 col-md-6 offset-md-3 col-lg-6 offset-lg-3 col-xl-4 offset-xl-4">
+                        <div class="login-brand">
+                            <img src="{{ asset('../assets/img/CP-Logo.png') }}" alt="logo" width="100" class="shadow-light rounded-circle">
                         </div>
 
-                        <div class="form-group row">
-                            <label for="password" class="col-md-4 col-form-label text-md-right">{{ __('Password') }}</label>
+                        <div class="card card-primary">
+                            <div class="card-header"><h4>Reset Password</h4></div>
 
-                            <div class="col-md-6">
-                                <input id="password" type="password" class="form-control @error('password') is-invalid @enderror" name="password" required autocomplete="new-password">
+                            <div class="card-body">
+                                <form method="POST" action="{{ route('password.update') }}">
+                                    @csrf
+                                    <input type="hidden" name="token" value="{{ $token }}">
+                                    <div class="form-group">
+                                        <label for="email">Email</label>
+                                        <input id="email" type="email" class="form-control @error('email') is-invalid @enderror" name="email" tabindex="1" autocomplete="email" required autofocus>
+                                        @error('email')
+                                            <span class="invalid-feedback" role="alert">
+                                                <strong>{{ $message }}</strong>
+                                            </span>
+                                        @enderror
+                                    </div>
 
-                                @error('password')
-                                    <span class="invalid-feedback" role="alert">
-                                        <strong>{{ $message }}</strong>
-                                    </span>
-                                @enderror
+                                    <div class="form-group">
+                                        <label for="password">New Password</label>
+                                        <input id="password" type="password" class="form-control pwstrength @error('password') is-invalid @enderror" data-indicator="pwindicator" name="password" tabindex="2" required>
+                                        <div id="pwindicator" class="pwindicator">
+                                            @error('password')
+                                                <span class="invalid-feedback" role="alert">
+                                                    <strong>{{ $message }}</strong>
+                                                </span>
+                                            @enderror
+                                        <div class="bar"></div>
+                                        <div class="label"></div>
+                                        </div>
+                                    </div>
+
+                                    <div class="form-group">
+                                        <label for="password-confirm">Confirm Password</label>
+                                        <input id="password-confirm" type="password" class="form-control" name="password_confirmation" tabindex="2" required>
+                                    </div>
+
+                                    <div class="form-group">
+                                        <button type="submit" class="btn btn-info btn-lg btn-block" tabindex="4">
+                                            Reset Password
+                                        </button>
+                                    </div>
+                                </form>
                             </div>
                         </div>
-
-                        <div class="form-group row">
-                            <label for="password-confirm" class="col-md-4 col-form-label text-md-right">{{ __('Confirm Password') }}</label>
-
-                            <div class="col-md-6">
-                                <input id="password-confirm" type="password" class="form-control" name="password_confirmation" required autocomplete="new-password">
-                            </div>
+                        <div class="simple-footer">
+                        Copyright &copy; Stisla 2018
                         </div>
-
-                        <div class="form-group row mb-0">
-                            <div class="col-md-6 offset-md-4">
-                                <button type="submit" class="btn btn-primary">
-                                    {{ __('Reset Password') }}
-                                </button>
-                            </div>
-                        </div>
-                    </form>
+                    </div>
                 </div>
             </div>
-        </div>
+        </section>
     </div>
-</div>
-@endsection
+
+<!-- General JS Scripts -->
+<script src="{{ asset('../assets/modules/jquery.min.js') }}"></script>
+<script src="{{ asset('../assets/modules/popper.js') }}"></script>
+<script src="{{ asset('../assets/modules/tooltip.js') }}"></script>
+<script src="{{ asset('../assets/modules/bootstrap/js/bootstrap.min.js') }}"></script>
+<script src="{{ asset('../assets/modules/nicescroll/jquery.nicescroll.min.js') }}"></script>
+<script src="{{ asset('../assets/modules/moment.min.js') }}"></script>
+<script src="{{ asset('../assets/js/stisla.js') }}"></script>
+
+<!-- JS Libraries -->
+
+<!-- Page Specific JS File -->
+
+<!-- Template JS File -->
+<script src="{{ asset('../assets/js/scripts.js') }}"></script>
+<script src="{{ asset('../assets/js/custom.js') }}"></script>
+</body>
+</html>
